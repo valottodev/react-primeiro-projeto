fe8b11b0579b7a3e802a6cc853b469246ca22aa8
valottodev <lucas.valotto@gmail.com>
2024-07-29 17:03:27 -0300

    1st commit

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,20 @@
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConcept";
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+        {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            /> */}
+      </ul>
+    </section>
+  );
+}
